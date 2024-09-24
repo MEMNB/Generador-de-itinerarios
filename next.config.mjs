4f3b9b51f0eea1ef0f049ed7d7c5b1d86d912154
Maria Eugenia <mariaeugenia.98@hotmail.com>
2024-09-24 20:18:52 +0200

    avoid supbase folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /supabase/,
+      loader: 'ignore-loader'
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
