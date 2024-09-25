de668c4d8df097cfea371780f424fccdbf049425
Maria Eugenia <mariaeugenia.98@hotmail.com>
2024-09-25 19:56:21 +0200

    asdf

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /supabase/,
-      loader: 'ignore-loader'
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
