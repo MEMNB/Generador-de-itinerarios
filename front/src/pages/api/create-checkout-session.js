88c76e19b77af2ab3490b8ef29ebea988ca8cce4
Maria Eugenia <mariaeugenia.98@hotmail.com>
2024-09-26 11:57:04 +0200

    changes session

--- front/src/pages/api/create-checkout-session.js
+++ front/src/pages/api/create-checkout-session.js
@@ -8,43 +8,45 @@ const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY
 const supabase = createClient(supabaseUrl, supabaseServiceKey)
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    try {
-      const { city, days, redirect_url } = req.body;
-      const amount = 100; // En centavos, por ejemplo, $10.00
-
-      const itineraryId = uuidv4();
-
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: [{
-          price_data: {
-            currency: 'eur',
-            product_data: {
-              name:  `Guía de viaje para ${city} por ${days} días`,
-            },
-            unit_amount: amount,
-          },
-          quantity: 1,
-        }],
-        mode: 'payment',
-        success_url: `${redirect_url}/r/${itineraryId}`,
-        cancel_url: `${redirect_url}?canceled=true`,
-      });
-
-      const { data, error } = await supabase
-        .from('itineraries')
-        .insert([
-          { id: itineraryId, days: days, city: city, stripe_session_id: session.id },
-        ])
-        .select()
-
-      res.status(200).json({ session_id: session.id, itineraryId: itineraryId });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  } else {
+  // Asegúrate de que la ruta API solo acepte solicitudes POST
+  if (req.method !== 'POST') {
     res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    return res.status(405).end(`Método ${req.method} no permitido`);
+  }
+
+  try {
+    const { city, days, redirect_url } = req.body;
+    const amount = 100; // En centavos, por ejemplo, $10.00
+
+    const itineraryId = uuidv4();
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          currency: 'eur',
+          product_data: {
+            name:  `Guía de viaje para ${city} por ${days} días`,
+          },
+          unit_amount: amount,
+        },
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: `${redirect_url}/r/${itineraryId}`,
+      cancel_url: `${redirect_url}?canceled=true`,
+    });
+
+    const { data, error } = await supabase
+      .from('itineraries')
+      .insert([
+        { id: itineraryId, days: days, city: city, stripe_session_id: session.id },
+      ])
+      .select()
+
+    res.status(200).json({ session_id: session.id, itineraryId: itineraryId });
+  } catch (error) {
+    console.error('Error en la creación de la sesión de pago:', error);
+    return res.status(500).json({ error: 'Error interno del servidor' });
   }
 }
