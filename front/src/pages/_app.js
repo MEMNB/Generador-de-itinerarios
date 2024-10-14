29d7fb5a6fbc9500e2d679a8dd7647da3cf33d32
Maria Eugenia <mariaeugenia.98@hotmail.com>
2024-10-14 20:07:02 +0200

    stripe

--- front/src/pages/_app.js
+++ front/src/pages/_app.js
@@ -10,6 +10,8 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
         <title>Ruta de Viaje - Tu Planificador de Viajes</title>
         <script defer data-domain="rutadeviaje.es" src="http://plausible-pwssk0kkokgo8ggo8kcw4wsc.157.90.124.158.sslip.io/js/script.js"></script>
+        <meta property="description" content="Ruta de Viaje es una plataforma que te permite planificar tus viajes. Genera un itinerario al instante ingresando la ciudad que desees y los días que planeas estar." />
+        <meta property="og:image" content="Link preview image URL"></meta>
       </Head>
       <Component {...pageProps} />
     </>
